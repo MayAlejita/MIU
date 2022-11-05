76230b84acfe739de2164fc7e8588c44dd9f7272
Mayra <mayra@Jimmys-MacBook-Pro.local>
2022-11-05 14:58:43 -0500

    three entries whose sum equal to a number

--- Practices/src/MPP_Pretest/datastructure/MainArrays.java
+++ Practices/src/MPP_Pretest/datastructure/MainArrays.java
@@ -1,2 +1,24 @@
-package MPP_Pretest.datastructure;public class MainArrays {
+package MPP_Pretest.datastructure;
+
+public class MainArrays {
+    public static void main(String[] args) {
+        int [] intArr = new int[]{5, 1, 23, 21, 17, 2, 3, 9, 12};
+        int testInt = 22;
+        System.out.println(testForSum(intArr, testInt));
+
+    }
+
+    public static boolean testForSum(int [] intArr, int testInt){
+        for(int i = 0; i < intArr.length; i ++){
+            for(int j = 0; j < intArr.length; j ++){
+                for(int k = 0; k < intArr.length; k ++){
+                    if(i != j && i!= k && j!=k && intArr [i] + intArr[j] + intArr[k] == testInt){
+                        System.out.println( "Numbers: " + intArr [i] + " - "+ intArr[j] + " - "+ intArr[k]);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }

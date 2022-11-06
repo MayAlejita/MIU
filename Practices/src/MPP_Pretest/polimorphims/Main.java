ec942d9a3f773afe686624a41b00d1420c9b5148
Mayra <mayra@Jimmys-MacBook-Pro.local>
2022-11-06 15:38:31 -0600

    example polimorphims

--- Practices/src/MPP_Pretest/polimorphims/Main.java
+++ Practices/src/MPP_Pretest/polimorphims/Main.java
@@ -1,5 +1,9 @@
 package MPP_Pretest.polimorphims;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -11,6 +15,23 @@ public class Main {
 
         System.out.println(student.printBirthday(1989));
         System.out.println(student.printBirthday(1989, 3));
+
+        List<Person> listPerson = new ArrayList<>();
+        Person person1 = new Person("Ana", "Lopez",12);
+        Person person2 = new Person("Luis", "Luna", 25);
+        listPerson.add(person1);
+        listPerson.add(person2);
+        listPerson.add(student);
+
+        System.out.println(listPerson);
+
+        Student student1 = new Student("Jose", "Perez", 22, "A");
+        Person person3 = student1;
+
+        //Student student2 = (Student) person1; //nooooo
+
+        System.out.println(person3);
+
 
 
 

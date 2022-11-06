d22ba270379b6b22d77a6e247a37d87123c6dde3
Mayra <mayra@Jimmys-MacBook-Pro.local>
2022-11-06 14:55:51 -0600

    Set data structure

---
+++ Practices/src/MPP_Pretest/datastructure/MainSet.java
@@ -0,0 +1,16 @@
+package MPP_Pretest.datastructure;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainSet {
+    public static void main(String[] args) {
+        Set<String> listSet = new HashSet<>();
+        listSet.add("dog");
+        listSet.add("dog");
+
+        System.out.println(listSet);
+    }
+
+
+}

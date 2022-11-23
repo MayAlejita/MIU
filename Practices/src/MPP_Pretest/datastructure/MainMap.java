5a9d150fa9513a884a3af4ad0291accea0bcf9c3
MayAlejita <mayrita.pullupaxi@gmail.com>
2022-11-23 17:15:11 -0600

    practices

--- Practices/src/MPP_Pretest/datastructure/MainMap.java
+++ Practices/src/MPP_Pretest/datastructure/MainMap.java
@@ -7,6 +7,7 @@ public class MainMap {
     public static void main(String[] args) {
         Map<String, Integer> ecuadorianPeople = new HashMap<>();
         ecuadorianPeople.put("Mayra", 33);
+        ecuadorianPeople.put("Mayra", 35);
         ecuadorianPeople.put("Jimmy", 34);
         System.out.println(ecuadorianPeople);
 
